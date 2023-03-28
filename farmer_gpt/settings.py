cf69f27069d93d5f722d8b71bc679bc9cfaabc17
Ankit Sharma <nez.dev@icloud.com>
2023-03-28 16:25:58 +0530

    removed key from settings

--- farmer_gpt/settings.py
+++ farmer_gpt/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-e2+6143r#k^d#5l0!f&mylaam_k_9hh07l%%f0+@p@gpzatm$4'
-OPENAI_API_KEY = "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"
+
+# load from .env
+load_dotenv()
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
